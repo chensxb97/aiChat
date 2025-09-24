16df08e9e63bbc23306703952f3ceed0d36e15de
Benedict <chensxb97@gmail.com>
2025-09-24 23:15:02 +0800

    init files

---
+++ README.md
@@ -0,0 +1,3 @@
+# aiChat
+A project that explores a basic ai chat app using [Vercel's AI SDK](https://ai-sdk.dev/docs/getting-started/nextjs-app-router) + [Mistral AI Provider](https://ai-sdk.dev/providers/ai-sdk-providers/mistral#mistral-ai-provider).
+
---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,29 @@
+import { mistral } from '@ai-sdk/mistral';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import { z } from 'zod';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 5;
+
+export async function POST(req: Request) {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+    const result = streamText({
+        model: mistral('magistral-small-2506'),
+        apiKey: process.env.MISTRAL_API_KEY,
+        prompt: convertToModelMessages(messages),
+        tools: {
+            weather: tool({
+                description: 'Get the weather in a location',
+                inputSchema: z.object({
+                    location: z.string().describe('The location to get the weather for'),
+                }),
+                execute: async ({ location }) => ({
+                    location,
+                    temperature: 72 + Math.floor(Math.random() * 21) - 10,
+                }),
+            }),
+        },
+    });
+
+    return result.toUIMessageStreamResponse();
+}
